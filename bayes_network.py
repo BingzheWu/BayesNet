5f9dfc5774b10346fadd99269f6eee1b61f592a0
bingzhe wu <wubingzheagent@gmail.com>
2018-09-30 16:48:18 +0800

    add BayesLeNet

--- bayes_network.py
+++ bayes_network.py
@@ -5,37 +5,7 @@ import numpy as np
 from utils import Gaussian, ScaleMixtureGaussian
 import math
 import torch.nn.functional as F
-class BayesLinearLayer(nn.Module):
-    def __init__(self, in_features, out_features, device = None):
-        super().__init__()
-        SIGMA_1 = torch.cuda.FloatTensor([math.exp(-0)])
-        SIGMA_2 = torch.cuda.FloatTensor([math.exp(-6)])
-        self.device = device
-        self.in_features = in_features
-        self.out_features = out_features
-        self.weight_mu = nn.Parameter(torch.Tensor(out_features, in_features).uniform_(-0.2, 0.2))
-        self.weight_rho = nn.Parameter(torch.Tensor(out_features, in_features).uniform_(-5, -4))
-        self.weight = Gaussian(self.weight_mu, self.weight_rho, device = self.device)
-        self.bias_mu = nn.Parameter(torch.Tensor(out_features).uniform_(-0.2, 0.2))
-        self.bias_rho = nn.Parameter(torch.Tensor(out_features).uniform_(-5,-4))
-        self.bias = Gaussian(self.bias_mu, self.bias_rho, device = self.device)
-        self.weight_prior = ScaleMixtureGaussian(0.5, SIGMA_1, SIGMA_2)
-        self.bias_prior = ScaleMixtureGaussian(0.5, SIGMA_1, SIGMA_2)
-        self.log_prior = 0
-        self.log_variational_posterior = 0
-
-    def forward(self, input, sample = False, calculate_log_probs = False):
-        if self.training or sample:
-            weight = self.weight.sample()
-            bias = self.bias.sample()
-        else:
-            weight = self.weight.mu
-            bias = self.bias.mu
-        if self.training or calculate_log_probs:
-            #self.log_prior = self.weight_prior.log_prob(weight)
-            self.log_variational_posterior = self.weight.log_prob(weight) + self.bias.log_prob(bias)
-            self.log_prior = self.weight_prior.log_prob(weight) + self.bias_prior.log_prob(bias)
-        return F.linear(input, weight, bias)
+from bayes_layers import BayesConvLayer, BayesLinearLayer
 class BayesMLP(nn.Module):
     def __init__(self, num_classes = 10, device = None):
         super().__init__()
@@ -74,5 +44,51 @@ class BayesMLP(nn.Module):
         loss = (log_variational_posterior - log_prior)/batch_size + negative_log_likelihood
         return loss, log_prior, log_variational_posterior, negative_log_likelihood
 
-
-        
+class BayesLeNet(nn.Module):
+    def __init__(self, num_classes, device = None):
+        super(BayesLeNet, self).__init__()
+        self.device = device
+        self.num_classes = num_classes
+        self.conv1 = BayesConvLayer(1, 6, 5, device = device)
+        self.conv2 = BayesConvLayer(6, 16, 5, device = device)
+        self.fc1 = BayesLinearLayer(16*5*5, 120, device = device)
+        self.fc2 = BayesLinearLayer(120, 84, device = device)
+        self.fc3 = BayesLinearLayer(84, 10, device = device)
+    def forward(self, x, sample = False):
+        x = F.relu(self.conv1(x, sample))
+        x = F.max_pool2d(x, 2)
+        x = F.relu(self.conv2(x, sample))
+        x = F.max_pool2d(x,2)
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc1(x, sample))
+        x = F.relu(self.fc2(x, sample))
+        x = self.fc3(x, sample)
+        x = F.log_softmax(x, dim = 1)
+        return x
+    def log_prior(self):
+        return self.conv1.log_prior \
+               + self.conv2.log_prior \
+               + self.fc1.log_prior \
+               + self.fc2.log_prior \
+               + self.fc3.log_prior
+    def log_variational_posterior(self):
+        return self.conv1.log_variational_posterior \
+               + self.conv2.log_variational_posterior \
+               + self.fc1.log_variational_posterior \
+               + self.fc2.log_variational_posterior \
+               + self.fc3.log_variational_posterior
+    
+    def sample_elbo(self, input, target, samples=2):
+        batch_size = input.size(0)
+        outputs = torch.zeros(samples, batch_size, self.num_classes).to(device = self.device)
+        log_priors = torch.zeros(samples).to(device = self.device)
+        log_variational_posteriors = torch.zeros(samples).to(device = self.device)
+        for i in range(samples):
+            outputs[i] = self(input, sample=True)
+            log_priors[i] = self.log_prior()
+            log_variational_posteriors[i] = self.log_variational_posterior()
+        log_prior = log_priors.mean()
+        log_variational_posterior = log_variational_posteriors.mean()
+        negative_log_likelihood = F.nll_loss(outputs.mean(0), target, size_average=False)
+        loss = (log_variational_posterior - log_prior)/batch_size + negative_log_likelihood
+        return loss, log_prior, log_variational_posterior, negative_log_likelihood
--- train.py
+++ train.py
@@ -2,7 +2,7 @@
 import torch
 from utils import write_loss_scalars
 from utils import write_weight_histograms
-from bayes_network import BayesMLP
+from bayes_network import BayesMLP, BayesLeNet
 from dataset import data_factory
 def train(net, optimizer, epoch, train_loader, device = None):
     net.train()
@@ -21,10 +21,11 @@ def main():
     print(device)
     train_loader = data_factory.make_dataloader('mnist', 100, is_train = True)
     net = BayesMLP(device = device).to(device)
+    net = BayesLeNet(10, device = device).to(device)
     optimizer = torch.optim.Adam(net.parameters())
     for epoch in range(20):
         train(net, optimizer, epoch, train_loader, device = device)
-    torch.save(net.state_dict(), 'checkpoint/mnist/bayes/bayesMlp.pth')
+    torch.save(net.state_dict(), 'checkpoint/mnist/bayes/bayesLeNet.pth')
 if __name__ == '__main__':
     main()
     
